7c90ff5e1cbc47cdb2b12a4fa8529eb33012a08b
patmood <patrick.n.moody@gmail.com>
2014-04-22 23:51:46 -0700

    send origin with form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express')
 var router = express.Router()
+  , airports = require('../airports')
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: 'Escape Plan' })
+})
+
+router.post('/origin', function(req, res) {
+  res.send(airports[req.body.origin])
 })
 
 module.exports = router
