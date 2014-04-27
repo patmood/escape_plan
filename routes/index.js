c10f761b3fd8422ab0fadf953b73ca9e5ac85b04
patmood <patrick.n.moody@gmail.com>
2014-04-27 16:41:30 -0700

    fetch prices with rome2rio api

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express')
 var router = express.Router()
   , airports = require('../airports')
   , distance = require('../lib/distance')
+  , getTrip = require('../lib/rome2rio')
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -9,14 +10,23 @@ router.get('/', function(req, res) {
 })
 
 router.post('/origin', function(req, res) {
-  distance(airports, req.body.origin.toUpperCase(), function(distArr) {
-    res.send(distArr)
+  var origin = req.body.origin.toUpperCase()
+  distance(airports, origin, function(dists) {
+    var dest = dists.summary.furthest[0]
+    getTrip(origin, dest, function(status, trip) {
+      console.log(status)
+      console.log(trip)
+      var details = (status === 200 && trip.routes.length > 0)
+        ? 'You\'re going to '
+        + airports[dest].city
+        + ', '
+        + airports[dest].country
+        + ' and it\'s going to cost you about $'
+        + trip.routes[0].indicativePrice.price
+        : 'Error'
+      res.send(details)
+    })
   })
 })
 
 module.exports = router
-
-
-// allDist(origin, function(x){
-//   console.log(x)
-// })
