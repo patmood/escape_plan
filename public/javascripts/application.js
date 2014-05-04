62aaebfa5e90ef307fd47cc86b555252f321fb88
patmood <patrick.n.moody@gmail.com>
2014-05-04 15:35:09 -0700

    return prices to font end

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,7 +4,7 @@ $('#origin-form').on('submit', function(e){
     dataType: 'json'
   , url: '/origin'
   , data: {
-      'origin': $('#origin-form input').val()
+      origin: $('#origin-form input').val()
     }
   , success: function(data) {
       console.log('success')
@@ -13,9 +13,27 @@ $('#origin-form').on('submit', function(e){
         'Closest travel: ' + data.summary.closest[0] + ', ' + data.summary.closest[1] + ' kms<br>'
       + 'Furthest travel: ' + data.summary.furthest[0] + ', ' + data.summary.furthest[1] + ' kms<br>'
       )
+      initPrices(data)
     }
   , error: function(err) {
       console.error(err)
     }
 	})
 })
+
+function initPrices(data) {
+  $.ajax({
+    dataType: 'json'
+  , url: '/price'
+  , data: {
+      origin: data.summary.closest[0]
+    , dest: data.summary.furthest[0]
+    }
+  , success: function(trips) {
+      console.log(trips)
+    }
+  , error: function(err) {
+      console.error(err)
+    }
+  })
+}
--- routes/index.js
+++ routes/index.js
@@ -9,27 +9,6 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Escape Plan' })
 })
 
- // **Deprecated
-// router.post('/origin', function(req, res) {
-//   var origin = req.body.origin.toUpperCase()
-//   distance(airports, origin, function(dists) {
-//     var dest = dists.summary.furthest[0]
-//     getTrip(origin, dest, function(status, trip) {
-//       console.log(status)
-//       console.log(trip)
-//       var details = (status === 200 && trip.routes.length > 0)
-//         ? 'You\'re going to '
-//         + airports[dest].city
-//         + ', '
-//         + airports[dest].country
-//         + ' and it\'s going to cost you about $'
-//         + trip.routes[0].indicativePrice.price
-//         : 'Error'
-//       res.send(details)
-//     })
-//   })
-// })
-
 router.get('/origin', function(req, res) {
   console.log(req.query)
   var origin = req.query.origin.toUpperCase()
@@ -38,5 +17,12 @@ router.get('/origin', function(req, res) {
   })
 })
 
+router.get('/price', function(req, res) {
+  console.log(req.query)
+  getTrip(req.query.origin, req.query.dest, function(err, trip) {
+    if (err) throw err
+    res.send(trip)
+  })
+})
 
 module.exports = router
