d3cc4ee1b209026caa8b1d29dbf094bdfcccadec
patmood <patrick.n.moody@gmail.com>
2014-05-13 21:10:47 -0700

    use underscore templating

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -14,15 +14,7 @@ $('#origin-form').on('submit', function(e){
       origin: $('#origin-form input').val()
     }
   , success: function(data) {
-      console.log('success')
-      console.log(data)
-      // $('.raw-data').append(
-      //   '<pre>' + JSON.stringify(data, undefined, 2) + '</pre>'
-      // )
-      data[0].segments.forEach(function(segment) {
-        console.log('SEGEMENT:', segment)
-        addSegment(segment)
-      })
+      data[0].segments.forEach(addSegment)
     }
   , error: function(err) {
       console.error(err)
@@ -31,25 +23,12 @@ $('#origin-form').on('submit', function(e){
 })
 
 function addSegment(details) {
-  if (details.kind === 'flight') {
-    var title = '<h2>' + capitalize(details.kind) + ' from ' + details.sCode + ' to ' + details.tCode + '</h2>'
-  } else {
-    var title = '<h2>' + capitalize(details.kind) + ' from ' + details.sName + ' to ' + details.tName + '</h2>'
-  }
   $('.itinerary').append(
-    title
-    + '<p>Price: ' + details.indicativePrice.price
-    + '<br>Distance: ' + details.distance + 'kms'
-    + '<br>Duration: ' + details.duration + ' minutes'
-    + '</p>'
+    _.template( $('#tpl-' + details.kind).html(), details)
   )
 }
 
 function reset() {
   $('.itinerary').empty()
   $('.raw-data').empty()
-}
-
-function capitalize(word) {
-  return word[0].toUpperCase() + word.slice(1)
 }
